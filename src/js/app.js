d32987eb87ba9cd76c4e184e32e905de1ce252f1
dave8git <dave8git@gmail.com>
2025-06-04 15:55:48 +0200

    Add factory added

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,10 @@ import { select, classNames } from './settings.js';
 class SongsList {
     constructor(dataService) {
         this.dataService = dataService;
+        console.log('!', this.dataService);
         this.renderDOM();
-        dataService.fetchSongs().then(() => {
-            this.initList(); 
+        this.dataService.fetchSongs().then((songs) => {
+            this.initList(songs); 
         }); 
     }
 
@@ -14,39 +15,47 @@ class SongsList {
         document.body.appendChild(this.container);
     }
 
-    initList() {
-        console.log(this.songs);
-        this.songs = this.dataService.songs;
-        for (let song of this.songs) {
-            SongFactory.create(song, this.container);
+    initList(songs) {
+        console.log(songs, this.container);
+        for (let song of songs) {
+            new SongPlayer(this.container, song);
         }
     }
 
     // here song search
 }
 
-class SongFactory {
-    static create(song, container) {
-        if (song.type === 'random') {
-            return new RandomSong(song, container);
-        } else {
-            return new SongPlayer(song, container);
-        }
-    }
+class Randomizer {  
+  static getRandom(data = []) {
+    const randomId = Math.floor((Math.random() * data.length));
+    
+    return data[randomId];
+  }
 }
 
-class RandomSong {
-    constructor(song, container) {
-        this.song = song;
-        this.container = container;
-        this.init();
-        //this.renderDOM();
+class SongPlayerFactory {
+    static create(container, song) {
+        const wrapper = SongPlayerFactory.createPlayerWrapper(song.filename)
+        container.appendChild(wrapper);
+        return wrapper;
     }
 
-    init() {
-        const message = document.createElement('p');
-        message.innerText = `🎲 Random pick: ${this.song.title}`;
-        this.container.appendChild(message);
+    static createPlayerWrapper(filename) {
+        const playerWrapper = document.createElement('div');
+        playerWrapper.classList.add('player');
+        playerWrapper.appendChild(SongPlayerFactory.createAudioElement(filename));
+        return playerWrapper;
+    }
+    static createAudioElement(filename) {
+        const audioElement = document.createElement('audio');
+        audioElement.appendChild(SongPlayerFactory.createAudioSourceElement(filename));
+        return audioElement;
+    }
+    static createAudioSourceElement(filename) {
+        const source = document.createElement('source');
+        source.src = `songs/${filename}`;
+        source.type = `audio/mpeg`;
+        return source;
     }
 }
 
@@ -54,30 +63,16 @@ class RandomSong {
 /* poczytać o dependency injection - piwnica prog.  */
 
 class SongPlayer {
-    constructor(song, container) {
+    constructor(container, song) {
         console.log('song', song);
         this.song = song; 
         this.container = container;
-        this.initSong();
+        this.initSong(song, container);
     }
 
     initSong() {
-        this.playerWrapper = document.createElement('div');
-        this.playerWrapper.classList.add('player');
-        const audioElement = document.createElement('audio');
-        this.playerWrapper.appendChild(audioElement);
-        const source = document.createElement('source');
-        source.src = `songs/${this.song.filename}`;
-        source.type = `audio/mpeg`;
-        audioElement.appendChild(source);
-        this.container.appendChild(this.playerWrapper);
-        new GreenAudioPlayer(this.playerWrapper);
-    }
-    createAudioElement() {
-        
-    }
-    createSourceForAudio() {
-
+        const player = SongPlayerFactory.create(this.container, this.song);
+        new GreenAudioPlayer(player);
     }
 }
 
@@ -92,7 +87,8 @@ class DataService { // search could be here but class has to be called from Song
                 return res.json(); 
             })
             .then(data => {
-                this.songs = data.songs;
+                
+                return data.songs;
             })
     }
 }
